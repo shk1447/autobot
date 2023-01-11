1d1ba1b2937d753bdbda1ad71b4ab77dfe7ee12d
석환 김 <sh2.kim@saigeresearch.ai>
2023-01-11 17:58:54 +0900

    마우스 이벤트는 연결 완료

--- main.js
+++ main.js
@@ -1,30 +1,27 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow, ipcMain } = require("electron");
+
+const _ = require("lodash");
+const Jimp = require("jimp");
 const path = require("path");
+const fs = require("fs-extra");
+const ioHook = require("iohook");
+const robot = require("@jitsi/robotjs");
 
-// const ioHook = require("iohook");
-
-// ioHook.on("mousemove", (event) => {
-//   console.log(event);
-// });
-
-// ioHook.on("keydown", (event) => {
-//   console.log(event);
-// });
-
-// ioHook.start();
-// ioHook.start(true);
-
+let mainWindow;
+let currentThrough = false;
 function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 500,
     height: 300,
     minWidth: 500,
     minHeight: 300,
+    transparent: true,
     kiosk: false,
     fullscreen: false,
-    resizable: false,
+    fullscreenable: true,
+    resizable: true,
     frame: false,
     show: true,
     webPreferences: {
@@ -37,8 +34,8 @@ function createWindow() {
       sandbox: false,
       enableRemoteModule: true,
     },
+    alwaysOnTop: true,
   });
-
   // and load the index.html of the app.
   mainWindow.loadFile("./dist/index.html");
 
@@ -68,7 +65,132 @@ app.on("window-all-closed", function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
 ipcMain.handle("exit", async (event, args) => {
-  console.log(event, args);
+  mainWindow.close();
   return true;
+});
+
+ipcMain.handle("click-through", async (event, args) => {
+  if (args.through) {
+    mainWindow.setIgnoreMouseEvents(true, { forward: true });
+    currentThrough = true;
+  } else {
+    mainWindow.setIgnoreMouseEvents(false, { forward: true });
+    currentThrough = false;
+  }
+});
+
+ipcMain.handle("capture", async (event, args) => {
+  const [x, y] = mainWindow.getPosition();
+  const [width, height] = mainWindow.getSize();
+
+  const bitmap = robot.screen.capture(x, y + 32, width, height - 64);
+  new Jimp(
+    { data: bitmap.image, width: bitmap.width, height: bitmap.height },
+    (err, image) => {
+      image.write("./test.png");
+    }
+  );
+});
+
+let hookArray = [];
+ioHook.useRawcode(false);
+
+ioHook.on("mousemove", (event) => {
+  hookArray.push(event);
+});
+ioHook.on("mouseclick", (event) => {
+  hookArray.push(event);
+});
+ioHook.on("mousedrag", (event) => {
+  hookArray.push(event);
+});
+ioHook.on("mousedown", (event) => {
+  hookArray.push(event);
+});
+ioHook.on("mouseup", (event) => {
+  hookArray.push(event);
+});
+ioHook.on("keydown", (event) => {
+  // console.log(event);
+});
+
+ioHook.on("keyup", (event) => {
+  hookArray.push(event);
+});
+
+ipcMain.handle("record", async (event, args) => {
+  if (args) {
+    hookArray = [];
+    ioHook.start();
+    ioHook.start(true);
+  } else {
+    ioHook.stop();
+  }
+});
+
+robot.setMouseDelay(0);
+
+const wait = (sec) => {
+  const start = Date.now();
+  let now = start;
+
+  while (now - start < sec * 1000) {
+    now = Date.now();
+  }
+};
+
+ipcMain.handle("play", async (event, args) => {
+  const clicks = hookArray.filter((d) => d.type == "mousedown");
+  hookArray.splice(hookArray.indexOf(clicks[clicks.length - 1]), 3);
+  for (var hook of hookArray) {
+    switch (hook.type) {
+      case "mousemove": {
+        wait(0.0001);
+        robot.moveMouse(hook.x, hook.y);
+        break;
+      }
+      case "mouseclick": {
+        wait(0.5);
+        const clickType =
+          hook.button == 1 ? "left" : hook.button == 2 ? "right" : "middle";
+        robot.mouseClick(clickType);
+        wait(0.5);
+        break;
+      }
+      case "mousedown": {
+        const clickType =
+          hook.button == 1 ? "left" : hook.button == 2 ? "right" : "middle";
+        robot.mouseToggle("down", clickType);
+        wait(0.5);
+        break;
+      }
+      case "mouseup": {
+        const clickType =
+          hook.button == 1 ? "left" : hook.button == 2 ? "right" : "middle";
+        robot.mouseToggle("up", clickType);
+        wait(0.5);
+        break;
+      }
+      case "mousedrag": {
+        wait(0.0001);
+        robot.dragMouse(hook.x, hook.y);
+        break;
+      }
+      case "keyup": {
+        if (hook.shiftKey || hook.altKey || hook.ctrlKey || hook.metaKey) {
+          console.log("short cut!!! gogo");
+        } else {
+          const text = String.fromCharCode(hook.rawcode);
+          robot.typeString(text.toLowerCase());
+        }
+        wait(0.0001);
+        break;
+      }
+      case "keydown": {
+        break;
+      }
+    }
+  }
 });
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,4 +1,5 @@
 import "./style.css";
+import "./w3.css";
 import { ipcRenderer } from "electron";
 import Alpine from "alpinejs";
 import "@fortawesome/fontawesome-free/js/all.js";
@@ -9,22 +10,59 @@ interface IHeader {
   exit: () => void;
 }
 
+interface IElectron {
+  isCapture: boolean;
+}
+
+interface IFooter {
+  snapshot: () => void;
+  start: () => void;
+}
+ipcRenderer.invoke("click-through", { through: false });
+
+Alpine.store("electron", {
+  isCapture: false,
+  toggleCapture() {
+    this.isCapture = !this.isCapture;
+    if (this.isCapture) {
+      (Alpine.store("footer") as any).toggleSetting(false);
+      ipcRenderer.invoke("record", true);
+    } else {
+      ipcRenderer.invoke("record", false);
+    }
+  },
+  through: async (value: boolean) => {
+    if ((Alpine.store("electron") as any).isCapture) {
+      await ipcRenderer.invoke("click-through", { through: value });
+    }
+  },
+});
+
 Alpine.store("header", {
-  title: "AUTO SYSTEM",
+  title: "AUTO CAPTURE SYSTEM",
   exit: async () => {
     const result = await ipcRenderer.invoke("exit");
     console.log(result);
   },
 } as IHeader);
-let count = 0;
-setInterval(() => {
-  const headerStore = Alpine.store("header") as IHeader;
-  headerStore.title = count.toString();
-  count++;
-}, 1000);
+// let count = 0;
+// setInterval(() => {
+//   const headerStore = Alpine.store("header") as IHeader;
+//   headerStore.title = count.toString();
+//   count++;
+// }, 1000);
 
 Alpine.store("footer", {
-  status: async () => {},
+  snapshot: async () => {
+    await ipcRenderer.invoke("capture");
+  },
+  play: async () => {
+    await ipcRenderer.invoke("play");
+  },
+  setting: true,
+  toggleSetting(val) {
+    this.setting = val != undefined ? val : !this.setting;
+  },
 });
 
 Alpine.start();
